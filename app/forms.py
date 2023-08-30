1697d5194e8e0d28a94c653c98770d6a952c7b00
Tiunov Vladimir <tiunov2008.va@gmail.com>
2023-08-30 18:32:34 +0300

    first commit

---
+++ app/forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SubmitField, SelectField, StringField, FileField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileAllowed, FileRequired
+
+class ReviewForm(FlaskForm):
+    name = StringField(
+        'Ваше имя',
+        validators=[DataRequired(message="Поле не должно быть пустым")]
+    )
+    text = TextAreaField(
+        'Текст',
+        validators=[DataRequired(message="Поле не должно быть пустым")])
+    score = SelectField(
+        'Оценка',
+    choices=[(i, i) for i in range(1, 11)])
+
+    submit = SubmitField('Добавить')
+
+
+class MovieForm(FlaskForm):
+    title = StringField(
+        'Название',
+        validators=[DataRequired(message="Поле не должно быть пустым")]
+    )
+    description = TextAreaField(
+        'Описание',
+        validators=[DataRequired(message="Поле не должно быть пустым")])
+    image = FileField(
+        'Изображение',
+        validators=[
+            DataRequired(message="Поле не должно быть пустым"),
+            FileAllowed(['png','jpg'], message="Неверный формат файла")
+        ])
+    submit = SubmitField('Добавить')
